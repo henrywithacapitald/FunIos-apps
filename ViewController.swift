7801595e1bfb64b5ceeec2e696f432ccbb439848
Henry <95125800+henrywithacapitald@users.noreply.github.com>
2021-12-18 22:21:39 +1300

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Henry Doyle on 16/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
